0fc0284065c9f7c84568c0c3079b17d18c7d0abe
Luca Zulberti <zulberti.luca@gmail.com>
2020-05-14 17:17:58 +0200

    common.mk: remove whitespaces
    Signed-off-by: Luca Zulberti <zulberti.luca@gmail.com>

--- common.mk
+++ common.mk
@@ -56,7 +56,7 @@ $(OUTDIR)/$1: $$($1_OBJS) $$($1_LIBS) | $(OUTDIR)
 	$(CC) $(LDFLAGS) -o $$@ $$(filter %.o,$$^) $$($1_LDFLAGS)
 	@echo Target $1 compiled successfully.
 
-ALL_OBJS   += $$($1_OBJS)
+ALL_OBJS += $$($1_OBJS)
 
 $1_clean:
 	rm -f $(OUTDIR)/$1
@@ -79,7 +79,7 @@ $(OUTDIR)/lib$1.a: $$($1_OBJS) | $(OUTDIR)
 	$(AR) cr $$@ $$^
 	@echo Library $1 archived successfully.
 
-ALL_OBJS   += $$($1_OBJS)
+ALL_OBJS += $$($1_OBJS)
 
 $1_clean:
 	rm -f $(OUTDIR)/lib$1.a
@@ -93,7 +93,7 @@ $(foreach target, $(TARGETS), $(eval $(call make-target,$(target))))
 $(foreach lib, $(LIBS), $(eval $(call make-lib,$(lib))))
 
 # Remove possible duplicates
-ALL_OBJS   := $(sort $(ALL_OBJS))
+ALL_OBJS := $(sort $(ALL_OBJS))
 
 $(ALL_OBJS): $(OBJDIR)/%.o : %.c | $(OBJDIRS)
 	$(CC) -I$(INCDIR) -MM -MP -MQ '$@' -MF $(patsubst %.o,%.d,$@) $<
